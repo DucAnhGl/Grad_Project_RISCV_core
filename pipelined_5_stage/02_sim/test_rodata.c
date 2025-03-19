4d00228b705fdf8ea991966f1251bf286653e6dc
linhhocaonhat <ho.linh.1609@gmail.com>
2025-03-19 15:10:37 +0700

    clean v1

--- pipelined_5_stage/02_sim/test_rodata.c
+++
@@ -1,9 +0,0 @@
-int main (void) {
-    const char *message = "Hello, this is a read-only string!\n";
-
-    while (1) {
-        // Infinite loop to keep the program running
-    }
-
-    return 0;  // Return from main (never actually reached in a bare-metal program)
-}
