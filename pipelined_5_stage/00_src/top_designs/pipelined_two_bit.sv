879db4b26654f3c9771afce9f38addf0694f4886
linhhocaonhat <ho.linh.1609@gmail.com>
2025-03-28 12:51:42 +0700

    CCCCcc

--- pipelined_5_stage/00_src/top_designs/pipelined_two_bit.sv
+++ pipelined_5_stage/00_src/top_designs/pipelined_two_bit.sv
@@ -98,8 +98,9 @@ module pipelined_two_bit #(
 /*==============================   IF STAGE   ==============================*/
     // Instruction mem
     imem inst_imem(
-        //.clk_i  (clk_i),
-        .addr_i (IF_pc),
+        .clk_i  (clk_i),
+        .rden_i (pc_wren),
+        .addr_i (IF_pcnext),
         .data_o (IF_instr)
     );
 
--- pipelined_5_stage/02_sim/run_sweep.sh
+++ pipelined_5_stage/02_sim/run_sweep.sh
@@ -8,5 +8,5 @@ echo "IPC Accuracy" > log.csv
 # Sweep values 2..12
 for WIDTH in $(seq 2 12); do
   echo "== Running with HISTORY_WIDTH=$WIDTH =="
-  HISTORY_WIDTH=$WIDTH make all PREDICTOR=1 HW=$WIDTH
+  HISTORY_WIDTH=$WIDTH make all PREDICTOR=$PREDICTOR HW=$WIDTH
 done
